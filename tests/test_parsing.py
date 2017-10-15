12e98e88f9c56a1bec8ea372a3ad6cd1265d9191
grahamegee <grahamegee@gmail.com>
2017-10-15 20:10:14 +0100

    decide on format for test runner

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -8,8 +8,17 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(f'{dir_path}/parser_tests.yaml') as f:
     test_data = yaml.load(f) 
 
-@pytest.mark.parametrize('input,expected',[
-   (test['input'], test['expected']) for test in test_data
+@pytest.mark.parametrize('cronline,expected',[
+   (test['cronline'], test['expected']) for test in test_data if not test.get('error')
 ])
-def test_parser(input, expected):
-    assert parse(input) == expected
+def test_parser_valid_cases(cronline, expected):
+    actual = parse(cronline)
+    assert parse(cronline) == expected
+
+@pytest.mark.parametrize('cronline,expected',[
+   (test['cronline'], test['expected']) for test in test_data if test.get('error')
+])
+def test_parser_syntax_errors(cronline, expected):
+    with pytest.raises(SyntaxError) as excinfo:
+        parse(cronline)
+    assert str(excinfo.value) == expected['value']
