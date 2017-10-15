14631d66df34defed851815b911de3cfb9122598
grahamegee <grahamegee@gmail.com>
2017-10-15 16:43:46 +0100

    add regex to separate time_date_fields from command

--- cronparser/parse.py
+++ cronparser/parse.py
@@ -1,7 +1,15 @@
 import sys
+import re
+
+# 5 non whitespace sequences separated by at least 1 space.
+TIME_DATE_FIELDS = re.compile(
+    r' *([^\s]+) +([^\s]+) +([^\s]+) +([^\s]+) +([^\s]+) +(.*)')
 
 
 def parse(cron_line):
+    *time_data_fields, command = re.search(TIME_DATE_FIELDS, cron_line).groups()
+    print(time_data_fields)
+    print(command)
     return {cron_line}
 
 
